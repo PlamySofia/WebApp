86be6d05033ab8e09457e13e344809ebf6d8839e
PlamySofia <plamysofia@abv.bg>
2019-01-17 09:34:26 +0200

    my first commit

---
+++ Index.php
@@ -0,0 +1,9 @@
+<?php
+$myName = $_GET["myName"]." Cyrilic";
+$myAge=$_GET["age"]+40;
+?>
+<html>
+    <p>
+My name is <?php echo $myName ?> and my age is <?php echo $myAge ?>
+    </p>
+</html>
---
+++ Product.php
@@ -0,0 +1,3 @@
+<html>
+TEST Product
+</html>
